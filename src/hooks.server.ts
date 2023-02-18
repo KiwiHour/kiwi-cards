4213db5fc020653487974fab542ea877c6568385
Matthew Basford <mbasford1@sheffield.ac.uk>
2023-02-18 01:45:06 +0000

    added maintenance page and adjusted hook accordingly

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,21 @@
-import type { Handle } from "@sveltejs/kit"
+import { redirect, type Handle } from "@sveltejs/kit"
 
 import { MongoClient, ServerApiVersion } from "mongodb";
-import { MONGODB_USERNAME, MONGODB_PASSWORD } from "$env/static/private";
+import { MONGODB_USERNAME, MONGODB_PASSWORD, MAINTENANCE } from "$env/static/private";
 import { Db } from "$lib/classes";
 
 const MONGO_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@kiwikluster.rlek0su.mongodb.net/?retryWrites=true&w=majority`
 const mongoClient = new MongoClient(MONGO_URI, { serverApi: ServerApiVersion.v1 })
 
 export const handle: Handle = async ({ event, resolve }) => {
+
+	if (MAINTENANCE == "0" && event.url.pathname == "/maintenance") {
+		throw redirect(302, "/")
+	}
+
+	if (MAINTENANCE == "1" && event.url.pathname !== "/maintenance") {
+		throw redirect(302, "/maintenance")
+	}
 
 	try {
 		await mongoClient.connect()
---
+++ src/routes/maintenance/+page.svelte
@@ -0,0 +1,37 @@
+<main>
+	<div id="wrapper">
+		<h1>Under Maintenance</h1>
+		<p>KiwiKards is currently under maintenance due to unexpected issues or preparations for a new update</p>
+	</div>
+</main>
+
+<style>
+
+	main {
+		display: flex;
+		flex-direction: row;
+	}
+
+	#wrapper {
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		transform: translateY(-15%);
+	}
+
+	h1, p {
+		text-align: center;
+		margin: 0;
+	}
+
+	h1 {
+		font-size: 2.5em;
+	}
+
+	p {
+		font-size: 1.3em;
+	}
+
+</style>
